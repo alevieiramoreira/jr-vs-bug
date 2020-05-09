29421bd4b8cf70ac7e93a735c4115c0c021c7ea7
alevieiramoreira <alessandra-vieira-moreira@hotmail.com>
2020-05-08 22:45:42 -0300

    ajusta padrão de components

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,9 @@
 import React, { ReactElement } from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Login(): ReactElement {
-  return <div>login</div>;
+  return <Container>login</Container>;
 }
 
 export default Login;
